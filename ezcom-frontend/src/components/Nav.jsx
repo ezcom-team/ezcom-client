d41e38d77fa68b5612fcfef1cb6bce5e2e5daa4e
saritpat <69256964+saritpat@users.noreply.github.com>
2023-10-23 20:14:13 +0700

    Change to new color

--- ezcom-frontend/src/components/Nav.jsx
+++ ezcom-frontend/src/components/Nav.jsx
@@ -35,7 +35,7 @@ function Nav() {
   }, [location]);
 
   return (
-    <nav className="bg-400 text-100 min-h-[60px] py-2 justify-center">
+    <nav className="bg-300 text-100 min-h-[60px] py-2 justify-center">
       <div className="flex w-[95%] justify-between">
         <div className="flex gap-7 ml-5 box">
         <Link
