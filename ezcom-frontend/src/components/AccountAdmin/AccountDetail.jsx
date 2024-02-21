2c3754c6e9b6b0fb24d09dd157f705a7d3fb54b6
Napong <gorngorn9876@gmail.com>
2024-02-21 21:01:44 +0700

    Revert "Merge commit '776b7b3fa1d2d407c91f3e0f94cd2bbc3e82df8d' into gorn-component"
    This reverts commit 80e1cc50976a0bb5b783e575ac0cf14eac7fc6a2, reversing
    changes made to 075ac1a4527d35f28fb458ef9964b89d96e26589.

--- ezcom-frontend/src/components/AccountAdmin/AccountDetail.jsx
+++ ezcom-frontend/src/components/AccountAdmin/AccountDetail.jsx
@@ -1,49 +1,52 @@
-import picProduct from "../../img/testPic.png";
+import picProduct from '../img/testPic.png'
 import { data } from "../../content/detail";
-import p2 from "../../img/p2.jpg";
+import p2 from '../img/p2.jpg'
 import { useState, useEffect } from "react";
 
+
+
 const AccountDetail = ({ order }) => {
-  return (
-    <div className="grid grid-cols-[15%_30%_15%_15%_10%] bg-300 text-200 py-3 mb-1 ">
-      <div className="flex pl-2 align-middle">
-        <div className="flex gap-2 my-auto text-2xl text-primary j">
-          <div className="flex justify-center">09128091</div>
-        </div>
-      </div>
+    return (
+        <div className="grid grid-cols-[15%_30%_15%_15%_10%] bg-300 text-200 py-3 mb-1 ">
+            <div className="flex align-middle pl-2">
+                <div className="my-auto flex gap-2 text-2xl text-primary j">
+                    <div className="flex justify-center">09128091</div>
+                </div>
+            </div>
+            
+            <div className="flex align-middle ">
+                <div className="h-20 ml-5">
+                    <img src={p2} className='max-h-full max-w-full' />
+                </div>
+                <div className="my-auto ">
+                    <div className='ml-2 text-xl'>
+                        Napong keha
+                    </div>
+                    
+                </div>
+            </div>
 
-      <div className="flex align-middle ">
-        <div className="h-20 ml-5">
-          <img src={p2} className="max-w-full max-h-full" />
-        </div>
-        <div className="my-auto ">
-          <div className="ml-2 text-xl">Napong keha</div>
-        </div>
-      </div>
+            <div className="flex align-middle">
+                <div className="my-auto text-100 text-2xl">
+                    <div className="">test@test.test</div>
+                </div>
+            </div>
 
-      <div className="flex align-middle">
-        <div className="my-auto text-2xl text-100">
-          <div className="">test@test.test</div>
-        </div>
-      </div>
+            <div className="flex align-middle">
+                <div className="my-auto flex gap-2">
+                    <div className="my-auto ">Admin</div>
+                </div>
+            </div>
 
-      <div className="flex align-middle">
-        <div className="flex gap-2 my-auto">
-          <div className="my-auto ">Admin</div>
-        </div>
-      </div>
+            <div className="flex align-middle">
+                <div className="my-auto flex gap-2 round">
+                    <a className="border-[2px] border-primary px-8 py-2 rounded-md hover:bg-orange-600" href='#'>
+                        Edit
+                    </a>
 
-      <div className="flex align-middle">
-        <div className="flex gap-2 my-auto round">
-          <a
-            className="border-[2px] border-primary px-8 py-2 rounded-md hover:bg-orange-600"
-            href="#"
-          >
-            Edit
-          </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 export default AccountDetail;
