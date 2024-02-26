2a0912a73f6e14eb9211ad3406873074d6dafd6a
saritpat <69256964+saritpat@users.noreply.github.com>
2024-02-26 13:31:31 +0700

    Submit edit product

--- ezcom-frontend/src/components/Modal/EditProductModal.jsx
+++ ezcom-frontend/src/components/Modal/EditProductModal.jsx
@@ -5,7 +5,7 @@ import { Loading } from "../Loading/Loading";
 export const EditProductModal = ({ open, item, setIsOpen }) => {
     const [data, setData] = useState([]);
     const [spec, setSpec] = useState([]);
-    const [selectedForm, setSelectedForm] = useState(null);
+    const [selectedForm, setSelectedForm] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -15,12 +15,17 @@ export const EditProductModal = ({ open, item, setIsOpen }) => {
                     `https://ezcom-backend-production-09b5.up.railway.app/products/${item.ID}`
                 );
                 const responseSpec = await axios.get(
-                    `https://ezcom-backend-production-09b5.up.railway.app/products/spec/${responseProduct.data.Type}/${responseProduct.data.Specs}`
+                    `https://ezcom-backend-production-09b5.up.railway.app/products/spec/${"Mouse"}/${
+                        responseProduct.data.Specs
+                    }`
                 );
 
                 setData(responseProduct.data);
+                removeColor();
                 setSpec(responseSpec.data);
-                setForm(responseProduct.data.Type);
+                if (responseProduct.data.Type === "mouse") {
+                    setSelectedForm(mouse);
+                }
                 setLoading(false);
             } catch (error) {
                 console.error("Fetch Error", error);
@@ -30,11 +35,19 @@ export const EditProductModal = ({ open, item, setIsOpen }) => {
         fetchData();
     }, [item.ID]);
 
-    const setForm = (type) => {
-        setSelectedForm(Mouse);
+    const removeColor = () => {
+        // // ลบข้อมูล file color ออกจากข้อมูล
+        // const newData = { ...data, Color: [] };
+        // // เซ็ตข้อมูลใหม่โดยใช้ setData()
+        // setData(newData);
+
+        console.log("🚀 ~ removeColor ~ newData:", data);
     };
 
-    const Mouse = [
+    console.log("🚀 ~ EditProductModal ~ itemID:", item.ID);
+    console.log("🚀 ~ EditProductModal ~ itemSpec:", item.Specs);
+
+    const mouse = [
         { id: "image", text: "Image", type: "file" },
         { id: "type", text: "Type", type: "text" },
         { id: "name", text: "Name", type: "text" },
@@ -50,81 +63,223 @@ export const EditProductModal = ({ open, item, setIsOpen }) => {
         { id: "shape", text: "Shape", type: "text" },
         { id: "height", text: "Height", type: "text" },
         { id: "width", text: "Width", type: "text" },
-        { id: "price", text: "Price", type: "text" },
-        { id: "quantity", text: "Quantity", type: "text" },
         // เพิ่ม label อื่น ๆ ตามต้องการ
     ];
 
     const DynamicForm = ({ labels, initialData, initialSpec }) => {
-        var combinedObj = Object.assign({}, initialData, initialSpec);
+        const combinedObj = Object.assign({}, initialData, initialSpec);
         const [formData, setFormData] = useState(combinedObj);
-        console.log("🚀 ~ DynamicForm ~ formData:", formData);
+        const [colors, setColors] = useState([]);
+
+        console.log("🚀 ~ DynamicForm ~ combinedObj:", combinedObj);
+        console.log("🚀 ~ DynamicForm ~ initialData:", initialData);
 
         const handleInputChange = (e) => {
-            const { name, value, files } = e.target;
-            if (name === "image") {
+            const { name, value, files, checked } = e.target;
+            if (name === "Image") {
+                console.log("🚀 ~ handleInputChange ~ name:", name);
                 setFormData({
                     ...formData,
-                    image: files[0],
+                    Image: files[0],
                 });
+            } else if (name === "Color") {
+                const checkboxArray = colors || []; // เรียกใช้ค่าที่เก็บไว้หรือให้เป็นอาร์เรย์ว่างถ้ายังไม่มี
+                let updatedArray;
+                if (checked) {
+                    updatedArray = [...checkboxArray, value]; // เพิ่มค่าเข้าไปในอาร์เรย์
+                } else {
+                    updatedArray = checkboxArray.filter(
+                        (color) => color !== value
+                    ); // ลบค่าที่ถูก uncheck ออกจากอาร์เรย์
+                }
+                setColors(updatedArray);
             } else {
                 setFormData({
                     ...formData,
                     [name]: value,
                 });
             }
+            console.log("🚀 ~ handleInputChange ~ :", formData["Color"]);
         };
 
         const handleSubmit = (e) => {
             e.preventDefault();
-            sendData(formData);
+
+            let data = new FormData(); // สร้าง FormData ใหม่
+
+            labels.map((label) => {
+                // temp = label.id;
+                console.log("check : ", label.id, formData[label.text]);
+                if (label.id === "color") {
+                    colors.forEach((element) => {
+                        data.append(label.id, element);
+                    });
+                } else data.append(label.id, formData[label.text]);
+            });
+            data.append("specs", item.Specs);
+            console.log("check : ", "specs", item.Specs);
+
+            // ส่ง FormData ไปยังเซิร์ฟเวอร์
+            console.log("before sens :", data);
+            sendData(data);
+            setIsOpen(false);
         };
+
+        const sendData = async (data) => {
+            console.log("form data :", data);
+            try {
+                console.log("color is " + " : " + data.color);
+                // สร้าง request ด้วย Axios
+
+                const response = await axios.put(
+                    `https://ezcom-backend-production-09b5.up.railway.app/products/${item.ID}`,
+                    data,
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                    }
+                );
+                console.log(response.data);
+            } catch (error) {
+                // ดำเนินการเมื่อมีข้อผิดพลาดในการส่ง request
+                console.error("Error sending request:", error);
+            }
+        };
+
+        console.log(formData.image);
 
         return (
             <div>
                 <form onSubmit={handleSubmit}>
-                    <div class="grid gap-6 mb-6 md:grid-cols-3">
-                        {labels.map((label) => (
-                            <div>
-                                <label
-                                    htmlFor={label.id}
-                                    class="block mb-2 text-sm font-medium text-200 dark:text-white"
-                                >
-                                    {label.text}
-                                </label>
-                                <input
-                                    id={label.id}
-                                    type={label.type}
-                                    name={label.text}
-                                    value={
-                                        label.id === "image"
-                                            ? ""
-                                            : formData[label.text]
-                                    }
-                                    class="bg-300 border border-gray-300 text-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder={label.id}
-                                    onChange={handleInputChange}
-                                    required
-                                />
+                    {loading ? (
+                        <Loading />
+                    ) : (
+                        <>
+                            <div class="grid gap-6 mb-6 md:grid-cols-3">
+                                {labels.map((label) => (
+                                    <div key={label.id}>
+                                        <label
+                                            htmlFor={label.id}
+                                            className="block mb-2 text-sm font-medium text-200 dark:text-white"
+                                        >
+                                            {label.text}
+                                        </label>
+                                        {label.id === "color" ? (
+                                            <div>
+                                                <input
+                                                    id={`${label.id}-red`}
+                                                    type="checkbox"
+                                                    name={label.text}
+                                                    value="red"
+                                                    onChange={handleInputChange}
+                                                />
+                                                <label
+                                                    htmlFor={`${label.id}-red`}
+                                                    className="mr-2"
+                                                >
+                                                    Red
+                                                </label>
+
+                                                <input
+                                                    id={`${label.id}-green`}
+                                                    type="checkbox"
+                                                    name={label.text}
+                                                    value="green"
+                                                    onChange={handleInputChange}
+                                                />
+                                                <label
+                                                    htmlFor={`${label.id}-green`}
+                                                    className="mr-2"
+                                                >
+                                                    Green
+                                                </label>
+
+                                                <input
+                                                    id={`${label.id}-blue`}
+                                                    type="checkbox"
+                                                    name={label.text}
+                                                    value="blue"
+                                                    onChange={handleInputChange}
+                                                />
+                                                <label
+                                                    htmlFor={`${label.id}-blue`}
+                                                    className="mr-2"
+                                                >
+                                                    Blue
+                                                </label>
+
+                                                <input
+                                                    id={`${label.id}-black`}
+                                                    type="checkbox"
+                                                    name={label.text}
+                                                    value="black"
+                                                    onChange={handleInputChange}
+                                                />
+                                                <label
+                                                    htmlFor={`${label.id}-black`}
+                                                    className="mr-2"
+                                                >
+                                                    Black
+                                                </label>
+
+                                                <input
+                                                    id={`${label.id}-white`}
+                                                    type="checkbox"
+                                                    name={label.text}
+                                                    value="white"
+                                                    onChange={handleInputChange}
+                                                />
+                                                <label
+                                                    htmlFor={`${label.id}-white`}
+                                                >
+                                                    White
+                                                </label>
+                                            </div>
+                                        ) : (
+                                            <input
+                                                id={label.id}
+                                                type={label.type}
+                                                name={label.text}
+                                                value={
+                                                    label.id === "image"
+                                                        ? formData[label.files]
+                                                        : formData[label.text]
+                                                }
+                                                className="bg-300 border border-gray-300 text-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="-"
+                                                onChange={handleInputChange}
+                                                required
+                                            />
+                                        )}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                        </>
+                    )}
+
+                    <div className="flex justify-center gap-5 mt-4">
+                        <button
+                            onClick={handleSubmit}
+                            className="w-40 p-2 transition bg-green-600 rounded hover:bg-green-700 text-200"
+                        >
+                            <span>Save</span>
+                        </button>
+                        <button
+                            onClick={closeModal}
+                            className="w-40 p-2 transition rounded bg-primary hover:bg-orange-700 text-200"
+                        >
+                            Cancel
+                        </button>
                     </div>
                 </form>
             </div>
         );
     };
 
-    const handleSubmit = () => {
-        createOrder();
-        setisBuyModalOpen(false);
-    };
-
     const closeModal = () => {
         setIsOpen(false);
     };
-
-    // console.log("🚀 ~ EditProductModal ~ data:", data);
-    // console.log("🚀 ~ EditProductModal ~ spec:", spec);
 
     return (
         <div>
@@ -144,43 +299,20 @@ export const EditProductModal = ({ open, item, setIsOpen }) => {
                                     />
                                 </div>
                                 <div className="flex gap-10">
-                                    <div className="flex flex-col justify-between">
-                                        <div>Item</div>
-                                    </div>
-                                    <div className="flex flex-col justify-between">
+                                    <div className="flex flex-col justify-between text-2xl">
                                         <div>{item.Name}</div>
                                     </div>
                                 </div>
                             </div>
                             <form>
                                 <div className="p-4 bg-300 rounded-sm">
-                                    {loading ? (
-                                        <Loading />
-                                    ) : (
-                                        <>
-                                            {selectedForm && (
-                                                <DynamicForm
-                                                    labels={selectedForm}
-                                                    initialData={data}
-                                                    initialSpec={spec}
-                                                />
-                                            )}
-                                        </>
+                                    {selectedForm && (
+                                        <DynamicForm
+                                            labels={selectedForm}
+                                            initialData={data}
+                                            initialSpec={spec}
+                                        />
                                     )}
-                                </div>
-                                <div className="flex justify-center gap-5 mt-4">
-                                    <button
-                                        onClick={handleSubmit}
-                                        className="w-40 p-2 transition bg-green-600 rounded hover:bg-green-700 text-200"
-                                    >
-                                        <span>Save</span>
-                                    </button>
-                                    <button
-                                        onClick={closeModal}
-                                        className="w-40 p-2 transition rounded bg-primary hover:bg-orange-700 text-200"
-                                    >
-                                        Cancel
-                                    </button>
                                 </div>
                             </form>
                         </div>
