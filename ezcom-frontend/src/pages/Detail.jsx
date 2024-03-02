9d40bc6dee28f401998b45c80c7fadaedcdfc201
Saritpat Niyomthong <saritpat.n@gmail.com>
2024-03-02 14:50:37 +0700

    show highest price, recent price (#86)

--- ezcom-frontend/src/pages/Detail.jsx
+++ ezcom-frontend/src/pages/Detail.jsx
@@ -9,8 +9,14 @@ function Detail() {
     const { id } = useParams();
     const [data, setData] = useState([]);
     const [spec, setSpec] = useState([]);
+    const [order, setOrder] = useState([]);
+    const [matchedOrder, setMatchedOrder] = useState([]);
+
+    const [highestPrice, setHighestPrice] = useState(0);
+    const [recentPrice, setRecentPrice] = useState(null);
 
     useEffect(() => {
+        const token = localStorage.getItem("access-token");
         async function fetchData() {
             try {
                 const responseProduct = await axios.get(
@@ -19,8 +25,36 @@ function Detail() {
                 const responseSpec = await axios.get(
                     `https://ezcom-backend-production-09b5.up.railway.app/products/spec/${responseProduct.data.Type}/${responseProduct.data.Specs}`
                 );
+                const responeOrder = await axios.get(
+                    `https://ezcom-backend-production-09b5.up.railway.app/order/buys/${id}`,
+                    {
+                        headers: {
+                            Authorization: token,
+                        },
+                    }
+                );
+                const responeMatchedOrder = await axios.get(
+                    `https://ezcom-backend-production-09b5.up.railway.app/order/matched-order`,
+                    {
+                        headers: {
+                            Authorization: token,
+                        },
+                    }
+                );
                 setData(responseProduct.data);
                 setSpec(responseSpec.data);
+                setOrder(responeOrder.data);
+                setMatchedOrder(responeMatchedOrder.data);
+                console.log(
+                    "🚀 ~ fetchData ~ responeMatchedOrder.data:",
+                    responeMatchedOrder.data
+                );
+                console.log(
+                    "🚀 ~ fetchData ~ responeOrder.data:",
+                    responeOrder.data
+                );
+                highestBuyOrder(responeOrder.data);
+                recentMatchedOrder(responeMatchedOrder.data);
             } catch (error) {
                 console.error("Fetch Error", error);
             }
@@ -28,13 +62,45 @@ function Detail() {
 
         fetchData();
     }, []);
+
     console.log(data);
+
+    const highestBuyOrder = (orders) => {
+        for (const order of orders) {
+            const currentPrice = order.Price;
+
+            if (currentPrice > highestPrice) {
+                setHighestPrice(currentPrice);
+            }
+        }
+    };
+
+    const recentMatchedOrder = (matchedOrders) => {
+        let mostRecentOrder = null;
+        let mostRecentDate = new Date(0);
+
+        for (const order of matchedOrders) {
+            const createdAt = new Date(order.CreatedAt);
+
+            if (createdAt > mostRecentDate) {
+                mostRecentDate = createdAt;
+                mostRecentOrder = order;
+            }
+        }
+
+        setRecentPrice(mostRecentOrder ? mostRecentOrder.Price : 0);
+    };
 
     // console.log(id)
     return (
         <div className="bg-back">
             <Nav />
-            <Productdetail product={data} spec={spec} />
+            <Productdetail
+                product={data}
+                spec={spec}
+                highestBuyOrder={highestPrice}
+                recentMatchedOrder={recentPrice}
+            />
             <BuyOrSellOrTrade pid={id} />
         </div>
     );
