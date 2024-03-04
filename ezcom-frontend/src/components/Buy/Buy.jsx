8736e11400fd131b9785399c62aac4cae2a5d1b7
Napong <gorngorn9876@gmail.com>
2024-03-04 19:47:43 +0700

    q

--- ezcom-frontend/src/components/Buy/Buy.jsx
+++ ezcom-frontend/src/components/Buy/Buy.jsx
@@ -45,7 +45,7 @@ const Buy = ({ pid }) =>{
                     No order
                 </div> : 
                 <div>
-                    {buyOrders.sort((a, b) => a.Price - b.Price).map((order) => (
+                    {buyOrders.sort((a, b) => b.Price - a.Price).map((order) => (
                         <BuyDetail conditions={order.Condition} buyerImg={order.Buyer_img} colors={order.Color} buyerName={order.Buyer_name} price={order.Price} />
                     ))}
                 </div>
