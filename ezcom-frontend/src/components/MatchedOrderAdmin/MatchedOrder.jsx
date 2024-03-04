197d71e2d4dc2da2c1f118c8a8c6fb4c1a53cc3a
Saritpat Niyomthong <saritpat.n@gmail.com>
2024-03-04 17:48:36 +0700

    Fix login hover & no drop down if not login, add color to admin page, add loading, add some styles (#91)
    * Fix login hover & nodrop down if not login
    
    * Add color, some styles
    
    * Add loading, add some styles

--- ezcom-frontend/src/components/MatchedOrderAdmin/MatchedOrder.jsx
+++ ezcom-frontend/src/components/MatchedOrderAdmin/MatchedOrder.jsx
@@ -1,52 +1,64 @@
 import axios from "axios";
 import OrderDetailAdmin from "./OrderDetailAndmin";
 import { useState, useEffect } from "react";
+import { Loading } from "../Loading/Loading";
 
 const MatchedOrder = () => {
-  const [order, setOrder] = useState([]);
+    const [order, setOrder] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const token = localStorage.getItem("access-token");
-        const response = await axios.get(
-          `https://ezcom-backend-production-09b5.up.railway.app/order/matched-order`,
-          {
-            headers: {
-              Authorization: token,
-            },
-          }
-        );
-        console.log(response.data);
-        setOrder(response.data);
-      } catch (error) {
-        console.error("Fetch Error", error);
-      }
-    }
-    fetchData();
-  }, []);
-  return (
-    <div className=" max-w-[85vw] mx-auto  p-2">
-      <div className="grid grid-cols-[40%_15%_10%_10%_15%] mb-4 text-200">
-        <div className="flex justify-center ml-5">Item</div>
-        <div className="">Price</div>
-        <div className="">Buyer</div>
-        <div className="">Seller</div>
-        <div className="">Time</div>
-      </div>
-      {order ? (
-        <div>
-          {order.map(order => (
-            <OrderDetailAdmin order={order} />
-          ))}
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const token = localStorage.getItem("access-token");
+                const response = await axios.get(
+                    `https://ezcom-backend-production-09b5.up.railway.app/order/matched-order`,
+                    {
+                        headers: {
+                            Authorization: token,
+                        },
+                    }
+                );
+                console.log(response.data);
+                setOrder(response.data);
+                setLoading(false)
+            } catch (error) {
+                console.error("Fetch Error", error);
+            }
+        }
+        fetchData();
+    }, []);
+    return (
+        <div className=" max-w-[75vw] mx-auto p-2">
+            <div className="grid grid-cols-[40%_15%_10%_10%_15%] mb-4 text-200">
+                <div className="flex justify-center ml-5">Item</div>
+                <div className="">Price</div>
+                <div className="">Buyer</div>
+                <div className="">Seller</div>
+                <div className="">Time</div>
+            </div>
+            {loading ? (
+                <div className="my-[120px] h-full overflow-y-scroll place-items-center">
+                    <Loading />
+                </div>
+            ) : (
+                <div>
+                    {order ? (
+                        <div>
+                            {order.map((order) => (
+                                <OrderDetailAdmin order={order} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="text-100 flex justify-center mt-[64px]">
+                            No sell order
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
-      ) : (
-        <div className="text-100 flex justify-center mt-[64px]">
-          No sell order
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default MatchedOrder;
