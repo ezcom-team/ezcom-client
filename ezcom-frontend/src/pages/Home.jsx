10d963ccb6e5b6922e58684dd3c833fceafdf502
saritpat <69256964+saritpat@users.noreply.github.com>
2024-03-28 16:24:01 +0700

    Delete image

--- ezcom-frontend/src/pages/Home.jsx
+++ ezcom-frontend/src/pages/Home.jsx
@@ -29,25 +29,6 @@ function Home() {
             </Link>
           </div>
           <div className="w-3/5 lg:w-2/5 flex justify-center items-center">
-            <img
-            id="p1"
-              src="https://media.discordapp.net/attachments/1161860132708286487/1212296896828416000/222.png?ex=65f15277&is=65dedd77&hm=024287a36ec8d20d13c2d48c0ca14fa58e6352c6545abcac1bee957fb0fd1777&=&format=webp&quality=lossless"
-              alt="Placeholder"
-              className="object-cover rounded-full"
-            />
-            <img
-            id="p2"
-              src="https://cdn.discordapp.com/attachments/1161860132708286487/1212112826035077160/unnamed.png?ex=65f0a709&is=65de3209&hm=da1fc732f498b177ed4be87a51f2695a296d03df5e90f2ddabe41fd471b561cc&"
-              alt="Placeholder"
-              className="object-cover rounded-lg "
-            />
-
-            <img
-            id="p3"
-              src="https://media.discordapp.net/attachments/1161860132708286487/1212296897214021664/111.png?ex=65f15277&is=65dedd77&hm=a160d257b714f18dca49b84fc1a78b1c41d3cb9dec25b6d31e69ea26824999c2&=&format=webp&quality=lossless"
-              alt="Placeholder"
-              className="object-cover rounded-full "
-            />
           </div>
         </div>
       </div>
