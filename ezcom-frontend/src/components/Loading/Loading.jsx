454833768f766f29a92621ca513e2e585eb5f127
sumetsm <107297356+sumetsm@users.noreply.github.com>
2024-04-22 09:38:38 +0700

    change-color (#101)
    Co-authored-by: sumetsm <“sumetssr@gmail.com”>

--- ezcom-frontend/src/components/Loading/Loading.jsx
+++ ezcom-frontend/src/components/Loading/Loading.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 export const Loading = () => {
-    return (
-        <div>
-            <div class="my-[120px] flex space-x-2 justify-center items-center dark:invert">
-                <span class="sr-only">Loading...</span>
-                <div class="h-8 w-8 bg-100 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-                <div class="h-8 w-8 bg-100 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-                <div class="h-8 w-8 bg-100 rounded-full animate-bounce"></div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      {/* <div class="my-[120px] flex space-x-2 justify-center items-center dark:invert"> */}
+      <div class="my-[120px] flex space-x-2 justify-center items-center">
+        {/* <span class="sr-only bg-100">Loading...</span> */}
+        <div class="h-8 w-8 bg-200 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div class="h-8 w-8 bg-200 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div class="h-8 w-8 bg-200 rounded-full animate-bounce"></div>
+      </div>
+    </div>
+  );
 };
